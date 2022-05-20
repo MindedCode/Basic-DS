e8d0edc98ea5cf87a42c8d9405355cf2127441b5
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2022-05-21 03:33:09 +0530

    This is a program of display function the array

---
+++ all array operations/dis_arr.c
@@ -0,0 +1,38 @@
+****/This is a program of display function the array/****
+#include<stdio.h>
+void displayArr(int arr[], int n)
+{
+     int i=0;
+     for( i=0; i<=n;i++)
+     {
+          printf("%d\n",arr[i]);
+     }
+     printf("\n");
+}
+int main(){
+     int arr[100]={12,55,77,87};
+     printf("Output of the program ::\n");
+     displayArr(arr,4);
+
+}
+
+Output of the program ::
+12
+55
+77
+87
+
+
+____________________________________________array of the Display function___________________________________________________
+
+
+
+
+
+
+
+
+
+
+
+-
