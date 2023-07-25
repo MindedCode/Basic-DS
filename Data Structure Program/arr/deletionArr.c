d7387b79443cec02f8100986605c35e07525708a
Adarsh Kumar <99128631+MindedCode@users.noreply.github.com>
2023-07-25 16:39:54 +0530

    Update deletionArr.c

--- Data Structure Program/arr/deletionArr.c
+++ Data Structure Program/arr/deletionArr.c
@@ -68,3 +68,102 @@ int main() {
 // 2 3 5
 // deleted element : 5
 // 2 3
+
+
+
+
+
+
+
+=========================================Second Part -======================================
+    /******************************************************************************
+
+                deleteion all operation in array
+
+*******************************************************************************/
+#include <stdio.h>
+
+void Traverse(int arr[], int size){
+    for(int i=0; i<size; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+void deleteFirst(int arr[], int size){
+    int d = arr[0];
+    // if (size >= 10) {
+    //     printf("\nArray is full. Cannot delete element.\n");
+    //     return;
+    // }
+    for(int i = 0; i <= size-1; i++){
+        arr[i] = arr[i+1];
+    }
+    
+    printf("Deleted element in this array : %d\n", d);
+}
+
+void deleteAtIndex(int arr[], int size, int position){
+    int d = arr[position];
+    for(int i=position; i<=size-1; i++){
+        arr[i] = arr[i+1];
+    }
+    
+    printf("Deleted element in this array : %d\n", d);
+    return;
+}
+
+void deleteAtEnd(int arr[], int *size){
+    // int d = arr[size-1];
+    // size--;
+    *size = *size - 1;
+    printf("Deleted element in this array : %d\n", arr[*size]);      /// used size array how to find nhi to last capacity vali element delete hoga
+    
+}
+
+int main()
+{
+    int arr[10] = {1,2,3,4,5}, element = 23, index = 3;
+    int arraySize = sizeof(arr) / sizeof(arr[0]);
+    // printf("%d ",arraySize);
+    printf("Original array :\n");
+    Traverse(arr, arraySize);
+    
+    printf("\ndeleteed element in First position");
+    deleteFirst(arr, arraySize);
+    // this arrsize is static so increase the arraySize +1
+    arraySize--;
+    Traverse(arr, arraySize);
+    
+    printf("\ndeleteed element At Index");
+    deleteAtIndex(arr, arraySize, index);
+    arraySize--;
+    Traverse(arr, arraySize);
+    
+    printf("\ndeleteed element in End position\n");
+    deleteAtEnd(arr, &arraySize);
+    // arraySize--;
+    Traverse(arr, arraySize);
+    
+    return 0;
+}
+
+
+
+Output is:
+
+// Original array :
+// 1 2 3 4 5 0 0 0 0 0 
+
+// deleteed element in First positionDeleted element in this array : 1
+// 2 3 4 5 0 0 0 0 0 
+
+// deleteed element At IndexDeleted element in this array : 5
+// 2 3 4 0 0 0 0 0 
+
+// deleteed element in End position
+// Deleted element in this array : 0
+// 2 3 4 0 0 0 0 
+
+
+
+
